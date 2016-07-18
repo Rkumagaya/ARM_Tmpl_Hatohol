988ed2adc6890e4542c85e0b9633c470fada00c3
rina.kumagaya <rina.kumagaya@miraclelinux.com>
2016-07-19 02:16:40 +0900

    Update install_hatohol.sh
    epel腐ってた

--- install_hatohol.sh
+++ install_hatohol.sh
@@ -3,8 +3,8 @@
 #install hatohol
 yum install -y unzip wget
 wget -P /etc/yum.repos.d/ http://project-hatohol.github.io/repo/hatohol-el7.repo
-wget -P /tmp/ http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-6.noarch.rpm
-rpm -ivh /tmp/epel-release-7-6.noarch.rpm
+wget -P /tmp/ http://dl.fedoraproject.org/pub/epel/7/x86_64/e/epel-release-7-7.noarch.rpm
+rpm -ivh /tmp/epel-release-7-7.noarch.rpm
 yum install -y hatohol-server-16.01
 yum install -y hatohol-web-16.01
 yum install -y mariadb-server
@@ -48,19 +48,9 @@ wget -P /opt/azure_trapper/ https://raw.githubusercontent.com/Rkumagaya/ARM_Tmpl
 wget -P /opt/azure_trapper/ https://raw.githubusercontent.com/Rkumagaya/ARM_Tmpl_Hatohol/master/Azure-reader.conf
 wget -P /usr/lib/systemd/system/ https://raw.githubusercontent.com/Rkumagaya/ARM_Tmpl_Hatohol/master/hap_fluentd.service
 wget -P /usr/lib/systemd/system/ https://raw.githubusercontent.com/Rkumagaya/ARM_Tmpl_Hatohol/master/azure_trapper.service
-
-curl -L https://toolbelt.treasuredata.com/sh/install-redhat-td-agent2.sh | sh
+wget -P /tmp wget http://packages.treasuredata.com.s3.amazonaws.com/2/redhat/7/x86_64/td-agent-2.3.0-0.el7.x86_64.rpm
+rpm -i /tmp/td-agent-2.3.0-0.el7.x86_64.rpm
 
 chmod a+x /opt/azure_trapper/start_hap_fluentd.sh
 chmod a+x /opt/azure_trapper/azure_trapper.py
 systemctl daemon-reload
-
-#rpm --import https://packages.treasuredata.com/GPG-KEY-td-agent
-#  cat >/etc/yum.repos.d/td.repo <<'EOF';
-#[treasuredata]
-#name=TreasureData
-#baseurl=http://packages.treasuredata.com/2/redhat/\$releasever/\$basearch
-#gpgcheck=1
-#gpgkey=https://packages.treasuredata.com/GPG-KEY-td-agent
-#EOF
-#yum install -y td-agent
